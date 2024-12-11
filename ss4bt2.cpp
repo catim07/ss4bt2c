58b83bd5be77f4bc26a2408aa1e81bd97cea0f8f
catim07 <cuongdfle@gmail.com>
2024-12-11 08:09:42 +0700

    Add files via upload

---
+++ ss4bt2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("moi ban nhap so de biet chan le: ");
+	scanf("%d",&a);
+	if(a%2==0){
+		printf("so cua ban la chan");
+	}else{
+		printf("so cua ban la le");
+	
+	}
+	return 0;
+}
